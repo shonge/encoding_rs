9f811ba05cbed3f539164bc5ca7e244f0bf34928
xiongjiwei <xiongjiwei1996@outlook.com>
2021-11-26 18:55:18 +0800

    [0xAA, 0xAF]

--- src/gb18030.rs
+++ src/gb18030.rs
@@ -153,8 +153,8 @@ impl Gb18030Decoder {
                     } else if first_minus_offset == 0x27 && (trail_minus_offset as usize) < GB2312_PINYIN.len() {
                         // [0xA8]
                         handle.write_bmp_excl_ascii(GB2312_PINYIN[trail_minus_offset as usize])
-                    } else if first_minus_offset.wrapping_sub(0x77) < 6 {
-                        // [0xF8, 0xFD]
+                    } else if first_minus_offset.wrapping_sub(0x77) < 6 || first_minus_offset.wrapping_sub(0x29) < 6 {
+                        // [0xF8, 0xFD], [0xAA,0xAF]
                         // Bottom PUA
                         return (DecoderResult::Malformed(2, 0),
                                     unread_handle_second.consumed(),
